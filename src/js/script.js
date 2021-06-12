8a276b5f904494630f89ea28e545ea652c4b2cce
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-06-12 22:30:30 +0530

    hero completed

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+luxy.init();
+
+let hamburger = document.querySelector('.hamburger');
+let navlinks = document.querySelector('.navlinks');
+
+let active = false;
+hamburger.addEventListener('click',()=>{
+    active =!active;
+    if(active){
+        navlinks.classList.add('active');
+    }else{
+        navlinks.classList.remove('active');
+    }
+})
